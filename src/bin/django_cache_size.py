479beda199205cb670bf7ea72c28fb9545160473
Luke Murphey <Luke.Murphey@gmail.com>
2022-02-22 13:07:35 -0800

    Updating to use the new modular input library

--- src/bin/django_cache_size.py
+++ src/bin/django_cache_size.py
@@ -3,107 +3,16 @@ from splunk.appserver.mrsparkle.lib.util import make_splunkhome_path
 import re
 import logging
 from logging import handlers
-import httplib
 import hashlib
-import socket
 import json
 import sys
 import time
 import os
 
-def setup_logger():
-    """
-    Setup a logger.
-    """
-    
-    logger = logging.getLogger('web_availability_modular_input')
-    logger.propagate = False # Prevent the log messages from being duplicated in the python.log file
-    logger.setLevel(logging.INFO)
-    
-    file_handler = handlers.RotatingFileHandler(make_splunkhome_path(['var', 'log', 'splunk', 'cache_size_modular_input.log']), maxBytes=25000000, backupCount=5)
-    formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
-    file_handler.setFormatter(formatter)
-    
-    logger.addHandler(file_handler)
-    
-    return logger
+path_to_mod_input_lib = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'modular_input.zip')
+sys.path.insert(0, path_to_mod_input_lib)
 
-logger = setup_logger()
-
-from modular_input import Field, FieldValidationException, ModularInput
-
-class PathField(Field):
-    """
-    Represents a file path.
-    """
-    
-    def to_python(self, value):
-        Field.to_python(self, value)
-        return value
-    
-    def to_string(self, value):
-        return value
-
-class DurationField(Field):
-    """
-    The duration field represents a duration as represented by a string such as 1d for a 24 hour period.
-    
-    The string is converted to an integer indicating the number of seconds.
-    """
-    
-    DURATION_RE = re.compile("(?P<duration>[0-9]+)\s*(?P<units>[a-z]*)", re.IGNORECASE)
-    
-    MINUTE = 60
-    HOUR   = 60 * MINUTE
-    DAY    = 24 * HOUR
-    WEEK   = 7 * DAY
-    
-    UNITS = {
-             'w'       : WEEK,
-             'week'    : WEEK,
-             'd'       : DAY,
-             'day'     : DAY,
-             'h'       : HOUR,
-             'hour'    : HOUR,
-             'm'       : MINUTE,
-             'min'     : MINUTE,
-             'minute'  : MINUTE,
-             's'       : 1
-             }
-    
-    def to_python(self, value):
-        Field.to_python(self, value)
-        
-        # Parse the duration
-        m = DurationField.DURATION_RE.match(value)
-
-        # Make sure the duration could be parsed
-        if m is None:
-            raise FieldValidationException("The value of '%s' for the '%s' parameter is not a valid duration" % (str(value), self.name))
-        
-        # Get the units and duration
-        d = m.groupdict()
-        
-        units = d['units']
-        
-        # Parse the value provided
-        try:
-            duration = int(d['duration'])
-        except ValueError:
-            raise FieldValidationException("The duration '%s' for the '%s' parameter is not a valid number" % (d['duration'], self.name))
-        
-        # Make sure the units are valid
-        if len(units) > 0 and units not in DurationField.UNITS:
-            raise FieldValidationException("The unit '%s' for the '%s' parameter is not a valid unit of duration" % (units, self.name))
-        
-        # Convert the units to seconds
-        if len(units) > 0:
-            return duration * DurationField.UNITS[units]
-        else:
-            return duration
-
-    def to_string(self, value):        
-        return str(value)
+from modular_input import Field, FieldValidationException, ModularInput, DurationField, FilePathField
 
 class DjangoCacheSize(ModularInput):
     """
@@ -121,11 +30,11 @@ class DjangoCacheSize(ModularInput):
                        'use_single_instance': "true"}
         
         args = [
-                PathField("path", "Django Cache Path", "The path to the directory used by Django for the cache", empty_allowed=False),
+                FilePathField("path", "Django Cache Path", "The path to the directory used by Django for the cache", empty_allowed=False, validate_file_existence=False),
                 DurationField("interval", "Interval", "The interval defining how often to perform the check; can include time units (e.g. 15m for 15 minutes, 8h for 8 hours)", empty_allowed=False)
                 ]
-        
-        ModularInput.__init__( self, scheme_args, args )
+
+        ModularInput.__init__(self, scheme_args, args, logger_name='cache_size_modular_input', logger_level=logging.INFO)
         
         if timeout > 0:
             self.timeout = timeout
@@ -157,180 +66,8 @@ class DjangoCacheSize(ModularInput):
         
         return self.output_event(data, stanza, index=index, source=source, sourcetype=sourcetype, unbroken=unbroken, close=close, out=out)
         
-    def create_event_string(self, data_dict, stanza, sourcetype, source, index, unbroken=False, close=False ):
-        """
-        Create a string representing the event.
-        
-        Argument:
-        data_dict -- A dictionary containing the fields
-        stanza -- The stanza used for the input
-        sourcetype -- The sourcetype
-        source -- The source field value
-        index -- The index to send the event to
-        unbroken -- 
-        close -- 
-        """
-        
-        # Make the content of the event
-        data_str   = ''
-        
-        for k, v in data_dict.items():
-            data_str += ' %s=%s' % (k, v)
-        
-        # Make the event
-        event_dict = {'stanza': stanza,
-                      'data' : data_str}
-        
-        
-        if index is not None:
-            event_dict['index'] = index
-            
-        if sourcetype is not None:
-            event_dict['sourcetype'] = sourcetype
-            
-        if source is not None:
-            event_dict['source'] = source
-        
-        event = self._create_event(self.document, 
-                                   params=event_dict,
-                                   stanza=stanza,
-                                   unbroken=False,
-                                   close=False)
-        
-        # If using unbroken events, the last event must have been 
-        # added with a "</done>" tag.
-        return self._print_event(self.document, event)
-        
-    def output_event(self, data_dict, stanza, index=None, sourcetype=None, source=None, unbroken=False, close=False, out=sys.stdout ):
-        """
-        Output the given even so that Splunk can see it.
-        
-        Arguments:
-        data_dict -- A dictionary containing the fields
-        stanza -- The stanza used for the input
-        sourcetype -- The sourcetype
-        source -- The source to use
-        index -- The index to send the event to
-        unbroken -- 
-        close -- 
-        out -- The stream to send the event to (defaults to standard output)
-        """
-        
-        output = self.create_event_string(data_dict, stanza, sourcetype, source, index, unbroken, close)
-        
-        out.write(output)
-        out.flush()
-        
-    @staticmethod
-    def get_file_path( checkpoint_dir, stanza ):
-        """
-        Get the path to the checkpoint file.
-        
-        Arguments:
-        checkpoint_dir -- The directory where checkpoints ought to be saved
-        stanza -- The stanza of the input being used
-        """
-        
-        return os.path.join( checkpoint_dir, hashlib.md5(stanza).hexdigest() + ".json" )
-        
     @classmethod
-    def last_ran( cls, checkpoint_dir, stanza ):
-        """
-        Determines the date that the analysis was last performed for the given input (denoted by the stanza name).
-        
-        Arguments:
-        checkpoint_dir -- The directory where checkpoints ought to be saved
-        stanza -- The stanza of the input being used
-        """
-        
-        fp = None
-        
-        try:
-            fp = open( cls.get_file_path(checkpoint_dir, stanza) )
-            checkpoint_dict = json.load(fp)
-                
-            return checkpoint_dict['last_run']
-    
-        finally:
-            if fp is not None:
-                fp.close()
-        
-    @classmethod
-    def needs_another_run(cls, checkpoint_dir, stanza, interval, cur_time=None):
-        """
-        Determines if the given input (denoted by the stanza name) ought to be executed.
-        
-        Arguments:
-        checkpoint_dir -- The directory where checkpoints ought to be saved
-        stanza -- The stanza of the input being used
-        interval -- The frequency that the analysis ought to be performed
-        cur_time -- The current time (will be automatically determined if not provided)
-        """
-        
-        try:
-            last_ran = cls.last_ran(checkpoint_dir, stanza)
-            
-            return cls.is_expired(last_ran, interval, cur_time)
-            
-        except IOError as e:
-            # The file likely doesn't exist
-            return True
-        
-        except ValueError as e:
-            # The file could not be loaded
-            return True
-        
-        # Default return value
-        return True
-    
-    @classmethod
-    def save_checkpoint(cls, checkpoint_dir, stanza, last_run):
-        """
-        Save the checkpoint state.
-        
-        Arguments:
-        checkpoint_dir -- The directory where checkpoints ought to be saved
-        stanza -- The stanza of the input being used
-        last_run -- The time when the analysis was last performed
-        """
-        
-        fp = None
-        
-        try:
-            fp = open( cls.get_file_path(checkpoint_dir, stanza), 'w' )
-            
-            d = { 'last_run' : last_run }
-            
-            json.dump(d, fp)
-            
-        except Exception:
-            logger.exception("Failed to save checkpoint directory") 
-            
-        finally:
-            if fp is not None:
-                fp.close()
-    
-    @staticmethod
-    def is_expired( last_run, interval, cur_time=None ):
-        """
-        Indicates if the last run time is expired based .
-        
-        Arguments:
-        last_run -- The time that the analysis was last done
-        interval -- The interval that the analysis ought to be done (as an integer)
-        cur_time -- The current time (will be automatically determined if not provided)
-        """
-        
-        if cur_time is None:
-            cur_time = time.time()
-        
-        if (last_run + interval) < cur_time:
-            return True
-        else:
-            return False
-        
-    @classmethod
-    def get_cache_size(cls, path):
+    def get_cache_size(cls, path, logger=None):
         """
         Get the size of a Django file cache.
         
@@ -338,12 +75,13 @@ class DjangoCacheSize(ModularInput):
         path -- The path of the Django cache.
         """
         
-        logger.debug('Performing cache size analysis, path="%s"', path)
+        if logger:
+            logger.debug('Performing cache size analysis, path="%s"', path)
         
         return cls.get_directory_size(path)
     
     @classmethod
-    def get_directory_size(cls, path):
+    def get_directory_size(cls, path, logger=None):
         """
         Get the size of a directory.
         
@@ -362,19 +100,21 @@ class DjangoCacheSize(ModularInput):
                     file_size_tmp = os.path.getsize( os.path.join(root, file) )
                     total_size = total_size + file_size_tmp
                     
-                    logger.debug("File size, %i, %s", file_size_tmp, os.path.join(root, file) )
+                    if logger:
+                        logger.debug("File size, %i, %s", file_size_tmp, os.path.join(root, file) )
         
         return total_size, number_of_files
         
     def run(self, stanza, cleaned_params, input_config):
-        
+        self.logger.debug("Running")
+
         # Make the parameters
         interval   = cleaned_params["interval"]
         path       = cleaned_params["path"]
         sourcetype = "django_file_cache_size"
         index      = cleaned_params["index"]
         source     = stanza
-        
+
         if self.needs_another_run( input_config.checkpoint_dir, stanza, interval ):
             
             # Perform the Django cache file size analysis
@@ -383,8 +123,14 @@ class DjangoCacheSize(ModularInput):
             # Send the event
             self.output_result( path, total_size, number_of_files, stanza, index=index, source=source, sourcetype=sourcetype, unbroken=True, close=True )
             
-            # Save the checkpoint so that we remember when we last 
-            self.save_checkpoint(input_config.checkpoint_dir, stanza, int(time.time()) )
+            # Get the time that the input last ran
+            last_ran = self.last_ran(input_config.checkpoint_dir, stanza)
+
+            # Save the checkpoint so that we remember when we last ran the input
+            self.save_checkpoint_data(input_config.checkpoint_dir, stanza,
+                                        {
+                                            'last_run' : self.get_non_deviated_last_run(last_ran, interval, stanza)
+                                        })
         
 if __name__ == '__main__':
     try:
